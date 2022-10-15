6c7f983b721b51ce94adeefdb2d8a9383d587782
Oskar Lelko <oskar.lelko@gmail.com>
2022-10-15 15:11:42 -0400

    NLP update

--- js-nlp/index.js
+++ js-nlp/index.js
@@ -7,7 +7,9 @@ const stream = fs.createReadStream("./data/qna.csv");
 const rl = readline.createInterface({ input: stream });
 let data = [];
 
-const manager = new NlpManager({ languages: ['en'], forceNER: true, nlu: { useNoneFeature: true } });
+const qnaDatabase = {}
+
+const manager = new NlpManager({ languages: ['en'], forceNER: true, nlu: { useNoneFeature: false } });
 
 rl.on("line", (row) => {
     data.push(row.split(","));
@@ -15,6 +17,8 @@ rl.on("line", (row) => {
  
 rl.on("close", () => {
     data.forEach(row => {
+        if (row[0] === "Department") return
+
         const category = row[0]
         const questions = row[1].split(';').map(q => q.trim())
         const questionTag = row[2]
@@ -23,6 +27,10 @@ rl.on("close", () => {
         // Define the answer
         manager.addAnswer('en', questionTag, answer);
         console.log(`Adding answer ${ questionTag }: ${ answer }`)
+        qnaDatabase[questionTag] = {
+            question: questions[0],
+            answer
+        }
 
         questions.forEach(question => {
             // Train the model on the questions leading to given answer
@@ -50,9 +58,31 @@ app.get('/query', async (req, res) => {
     const response = await manager.process('en', query);
     console.log(response);
 
+    let topPossibility = null
+    response.classifications.forEach(possibility => {
+        if (!topPossibility) topPossibility = possibility
+
+        if (possibility.score > topPossibility.score) {
+            topPossibility = possibility
+        }
+    })
+
+    let answer = null
+
+    if (topPossibility.score > 0.15) {
+        answer = qnaDatabase[topPossibility.intent]
+    }
+
     return res.json({
-        success: response.answer ? true : false,
-        answer: response.answer
+        success: answer ? true : false,
+        intent: topPossibility,
+        answer,
+        classifications: response.classifications.map(c => {
+            return {
+                classification: c,
+                qna: qnaDatabase[c.intent]
+            }
+        })
     })
 })
 
